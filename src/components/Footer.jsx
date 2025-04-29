21f71a74b7c7514db1808b490e6987062be5eda7
KaifSoomro <kaifsoomro82@gmail.com>
2025-04-29 21:01:29 +0500

    footer-updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from "../assets/logo.png"
 
 const Footer = () => {
   return (
-    <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
-       <div className='flex items-center justify-center gap-4'>
-       <Link>About</Link>
-       <Link>Contact</Link>
-       </div>
-       <p className='text-sm'>Created by Kaif Soomro</p>
+    <footer className='w-full h-[300px]'>
+      <div className='container h-full mx-auto bg-gradient-to-tl from-neutral-950 to-neutral-800 py-5 px-3 flex items-center justify-center flex-col rounded'>
+          <img src={Logo} className='sm:w-[200px] w-[150px]' />
+          <p className='sm:text-xl mb-2'>The world of cinema, in your pocket.</p>
+          <div className='sm:block flex flex-col items-center justify-center'>
+            <input 
+            type="text" 
+            placeholder='Sign in to your account'
+            className='w-[300px] h-[45px] py-5 ps-3 mb-3 border rounded outline-orange-300'
+            />
+            <button className='w-[300px] sm:w-[100px] h-[45px] sm:ms-2 rounded bg-gradient-to-tl from-orange-800 to-yellow-500 cursor-pointer'>Sign in</button>
+          </div>
+      </div>
     </footer>
   )
 }
