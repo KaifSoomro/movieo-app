b48b11a197d382942419cafef2a37d7b284ebce2
KaifSoomro <kaifsoomro82@gmail.com>
2025-05-01 16:53:21 +0500

    sign-in

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,20 +10,33 @@ import { ToastContainer, toast } from 'react-toastify';
 const Footer = () => {
   const [ inputValue, setInputValue ] = useState("")
   const [ username, setUserName ] = useState([]);
-  const successNotify = () => toast.success('Signed in.', {
+  const [ signIn, setSignIn ] = useState(true)
+  const successNotify = () => toast.success(`Signed in as ${inputValue}`, {
     theme:"dark"  
   });
   const warnNotify = () => toast.error('Enter a username first.', {
     theme:"dark"  
   });
+  const LogOutNotify = () => toast.error('Logged out', {
+    theme:"dark"  
+  });
 
   const handleSubmit = () => {
+    username.push(inputValue)
+
     if(inputValue == ""){
       warnNotify()
     }else {
       successNotify()
+      localStorage.setItem("username", JSON.stringify(username))
+      setSignIn(false)
     }
     setInputValue("")
+  }
+
+  const handleLogout = () => {
+    setSignIn(true)
+    LogOutNotify()
   }
 
   return (
@@ -32,18 +45,27 @@ const Footer = () => {
           <img src={Logo} className='sm:w-[200px] w-[150px]' />
           <p className='sm:text-xl mb-2'>The world of cinema, in your pocket.</p>
           <div className='sm:block flex flex-col items-center justify-center'>
-            <input 
+          {
+            signIn ? "" : <button
+            onClick={handleLogout}
+            className='w-[300px] sm:w-[100px] h-[45px] sm:ms-2 rounded bg-gradient-to-tl from-red-800 to-red-400 cursor-pointer'
+            >Log out</button>
+          }
+          {
+            signIn && <input 
             type="text"
             value={inputValue} 
             placeholder='Enter your username'
             className='w-[300px] h-[45px] py-5 ps-3 mb-3 border rounded outline-orange-300'
             onChange={(e)=>setInputValue(e.target.value)}
            />
-            <button 
-              onClick={handleSubmit}
-              className='w-[300px] sm:w-[100px] h-[45px] sm:ms-2 rounded bg-gradient-to-tl from-orange-800 to-yellow-500 cursor-pointer'>
+          }
+           {
+            signIn && <button onClick={handleSubmit}
+            className='w-[300px] sm:w-[100px] h-[45px] sm:ms-2 rounded bg-gradient-to-tl from-orange-800 to-yellow-500 cursor-pointer'>
               Sign in
             </button>
+           }
           </div>
           <div className='flex items-center justify-center gap-5 mt-3'>
              <Link to={"https://www.instagram.com/kaif__soomro/"}>
